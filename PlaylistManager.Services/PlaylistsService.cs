02d77f5868285c456bf7abfe7da7c7ba880f2bb4
a-mateva <angelina.mateva@gmail.com>
2018-03-02 15:26:59 +0200

    Create playlists service class and scaffold controller

---
+++ PlaylistManager.Services/PlaylistsService.cs
@@ -0,0 +1,64 @@
+﻿using PlaylistManager.DataAccess;
+using PlaylistManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaylistManager.Services
+{
+    public class PlaylistsService
+    {
+        private UnitOfWork unitOfWork;
+
+        public PlaylistsService(UnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public bool Create(Playlist playlist)
+        {
+            try
+            {
+                unitOfWork.PlaylistsRepository.Add(playlist);
+                unitOfWork.Save();
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public bool Update(Playlist playlist)
+        {
+            try
+            {
+                unitOfWork.PlaylistsRepository.Add(playlist);
+                unitOfWork.Save();
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void Delete(Playlist playlist)
+        {
+            unitOfWork.PlaylistsRepository.Delete(playlist);
+            unitOfWork.Save();
+        }
+
+        public List<Playlist> GetAll()
+        {
+            return unitOfWork.PlaylistsRepository.GetAll();
+        }
+
+        public Playlist GetById(int id)
+        {
+            return unitOfWork.PlaylistsRepository.GetById(id);
+        }
+    }
+}
---
+++ PlaylistManager.Web/Controllers/PlaylistsController.cs
@@ -0,0 +1,25 @@
+﻿using PlaylistManager.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PlaylistManager.DataAccess;
+
+namespace PlaylistManager.Web.Controllers
+{
+    public class PlaylistsController : Controller
+    {
+        private PlaylistsService service;
+
+        public PlaylistsController()
+        {
+            service = new PlaylistsService(new UnitOfWork());
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
